ecfee9c924fc6f2d90a346796236537b5d5cb939
21040738-Tan-Liu-Yue <106258957+21040738-Tan-Liu-Yue@users.noreply.github.com>
2022-08-07 19:45:21 +0800

    Update LunchBox.java

--- C206_CaseStudy/src/LunchBox.java
+++ C206_CaseStudy/src/LunchBox.java
@@ -1,7 +1,5 @@
 
 public class LunchBox {
-<<<<<<< HEAD
-
 	private int id;
 	private String date;
 	private String meal;
@@ -58,58 +56,4 @@ public class LunchBox {
 	}
 
 }
-=======
-	 private int id;
-	  private String date;
-	  private String meal;
-	  private String drink;
-	  private String fruit;
-	  
-	  public LunchBox(int id, String date, String meal, String drink, String fruit) {
-	    this.id = id;
-	    this.date = date;
-	    this.meal = meal;
-	    this.drink = drink;
-	    this.fruit = fruit;
-	    
-	  }
-	  public int getId() {
-	    return id;
-	  }
-	  public void setId(int id) {
-	    this.id = id;
-	  }
-	  public String getDate() {
-	    return date;
-	  }
-	  public void setDate(String date) {
-	    this.date = date;
-	  }
-	  
-	  public String getMeal() {
-	    return meal;
-	  }
-	  public void setMeal(String meal) {
-	    this.meal = meal;
-	  }
-	  public String getDrink() {
-	    return drink;
-	  }
-	  public void setDrink(String drink) {
-	    this.drink = drink;
-	  }
-	  public String getFruit() {
-	    return fruit;
-	  }
-	  public void setFruit(String fruit) {
-	    this.fruit = fruit;
-	  }
-	  
 
-
-	}
-
-
-
-
->>>>>>> branch 'master' of https://github.com/21040738-Tan-Liu-Yue/C206_CaseStudy.git
